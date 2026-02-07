72d72076638f0b7cf001b96b90052eda0132c4a8
supreMe7492 <supremeraut94@gmail.com>
2026-02-07 09:34:31 +0545

    adding views

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/",(req,res)=>{
     res.send("<h1>pussy</h1>");
 })
